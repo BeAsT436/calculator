5e06ea2972b0c66e66b0c784fefdb48d7b87bd6d
Counter <yuriryabenko436@gmail.com>
2023-07-30 12:46:38 +0300

    removed bugs

--- js/main.js
+++ js/main.js
@@ -3,11 +3,24 @@ const result = document.querySelector(".result")
 const equal = document.querySelector(".equal")
 const reset = document.querySelector(".reset")
 
+function calculate(expression) {
+    try {
+    const regex = /[^0-9+\-*/.]/g
+    if(regex.test(expression)){
+        throw new Error("Invalid characters")
+    }
+    return Function(`"use strict";return (${expression});`)()
+    } catch (error) {
+        console.error("Error calculating",error)
+        return "Error"
+    }
+}
+
 equal.addEventListener("click", ()=>{
-    result.textContent = eval(result.textContent)
+    result.textContent = calculate(result.textContent)
 })
 reset.addEventListener("click", ()=>{
-    result.textContent = 0
+    result.textContent = ""
 })
 btns.forEach((btn)=>{
     btn.addEventListener("click", (e)=>{
